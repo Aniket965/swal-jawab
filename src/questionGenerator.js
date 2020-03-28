cfb675743345b7d33ae7a8b12336f5a55e1cee6c
aniket965 <aniket965.as@gmail.com>
2020-03-28 15:42:03 +0530

    feat: more questions

--- src/questionGenerator.js
+++ src/questionGenerator.js
@@ -5,6 +5,14 @@ const questions = [
     "where do you find $$ in another 10 years", 
     "Who is there in $$ ’s emergency contact", 
     "What is the first thing as a baby $$ had?",
+    " What is $$'s guilty pleasure?",
+    "Who is the last person that $$ stalked on social media?",
+    "Why did $$'s last relationship end?",
+    "What habit can’t $$ seem to quit?",
+    "What is $$'s secret talent if they have one?",
+    "What would $$ do with a million dollars?",
+    "What is the most romantic thing that $$ has ever done?",
+    "What is $$'s worst childhood memory?"
 ]
 
 function randomitem(myArray) {
