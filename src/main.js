e4eb55139d602af60d3d31ffa5c4ee0b9e70a983
aniket965 <aniket965.as@gmail.com>
2020-06-26 20:51:08 +0530

    feat:vue-analytics

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import { store } from './store.js'
 import router from "./router/index";
 import vuetify from './plugins/vuetify';
 
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-99011528-9',
+  router  
+})
 Vue.config.productionTip = false;
 
 
